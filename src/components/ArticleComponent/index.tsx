2a1371d86c6754f0fa89e6691f754526e7f81051
liangcai <cail1844@gmail.com>
2020-05-13 06:55:01 +0800

    use custom effect hook to fetch api data

--- src/components/ArticleComponent/index.tsx
+++ src/components/ArticleComponent/index.tsx
@@ -2,38 +2,15 @@ import React, { useState, useEffect } from "react";
 import { Grid } from "semantic-ui-react";
 import MenuTabularOnLeft from "./MenuTabularOnLeft";
 import TabularSegment from "./TabularSegment";
-import { getLists } from "../../hooks/useLists";
-import { getEntries } from "../../hooks/useEntries";
-import { AxiosResponse } from "axios";
+import { useLists } from "../../hooks/useLists";
+import { useEntries, useActiveItem } from "../../hooks/useEntries";
 
 const Article: React.FC = () => {
-  const [entriesLists, setEntriesLists] = useState<Array<any>>([]);
-  const [activeItem, setActiveItem] = useState<string>("");
-  const [entries, setEntries] = useState<Array<any>>([]);
+  const { entriesLists } = useLists();
+  const { activeItem, setActiveItem, entries, setEntries } = useEntries();
 
-  useEffect(() => {
-    getLists().then((response: AxiosResponse) => {
-      const lists = response.data.lists;
-      setEntriesLists(lists);
-
-      const defaultName =
-        lists.length === 0 ? "" : lists[lists.length - 1].id.toString();
-      setActiveItem(defaultName);
-      console.log("effect lists: ", lists);
-    });
-  }, []);
-
-  useEffect(() => {
-    console.log("start useeffect, activeItem: ", activeItem);
-    if (activeItem != "") {
-      const data = getEntries(activeItem);
-      setEntries(data);
-    }
-
-    // return () => {
-    //   console.log("return useeffect");
-    // };
-  }, [activeItem]);
+  // 设置默认选中的目录项
+  useActiveItem(entriesLists, setActiveItem);
 
   return (
     <article>
--- src/hooks/useLists.tsx
+++ src/hooks/useLists.tsx
@@ -1,12 +1,30 @@
+import { useState, useEffect } from "react";
 import { API_PREFIX } from "../config";
-import { AxiosPromise, AxiosResponse } from "axios";
+import { AxiosResponse } from "axios";
 
 const axios = require("axios").default;
 
+const useLists = () => {
+  const [entriesLists, setEntriesLists] = useState<Array<any>>([]);
+
+  useEffect(() => {
+    getLists().then((response: AxiosResponse) => {
+      const lists = response.data.lists;
+      setEntriesLists(lists);
+    });
+  }, []);
+
+  return {
+    entriesLists,
+    setEntriesLists,
+  };
+};
+
 const getLists = () => {
+  console.log("get lists");
   return axios.get(API_PREFIX + "lists/").then((response: AxiosResponse) => {
     return response;
   });
 };
 
-export { getLists };
+export { useLists };
