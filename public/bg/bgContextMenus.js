b0d9c5ab662a8c440b612366d05447482f28892a
liangcai <cail1844@gmail.com>
2020-05-14 15:36:31 +0800

    add submenu test

--- public/bg/bgContextMenus.js
+++ public/bg/bgContextMenus.js
@@ -7,8 +7,30 @@ chrome.runtime.onInstalled.addListener(function () {
   });
 });
 
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.contextMenus.create({
+    id: "parentItem",
+    type: "normal",
+    title: "save to selected list",
+    contexts: ["page"],
+  });
+  createMenu("sub1", "list1");
+  createMenu("sub2", "list2");
+});
+
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
   if (info.menuItemId === "goReadingList") {
     window.open(chrome.extension.getURL("index.html"));
   }
 });
+
+const createMenu = (id, title) => {
+  const options = {
+    id,
+    type: "normal",
+    title,
+    contexts: ["page"],
+    parentId: "parentItem",
+  };
+  chrome.contextMenus.create(options);
+};
